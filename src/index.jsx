3875b70876e6a4c2a05e538541a98a2e0fc5357e
Redvan <redvanisation@gmail.com>
2019-12-30 15:50:25 -0500

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/main.scss';
+
+const App = () => (
+  <h1>Hello World!</h1>
+);
+
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root'),
+);
