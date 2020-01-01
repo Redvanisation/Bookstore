6a90b05df88cfcd4ae4086d3f6508bedcfea92f8
Redvan <redvanisation@gmail.com>
2019-12-31 23:50:22 -0500

    Linting the code

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-parens */
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import { connect } from 'react-redux';
@@ -14,12 +15,12 @@ const BookList = ({ books }) => (
       </tr>
     </thead>
     <tbody>
-      {books.map((book) => <Book key={book.bookId} book={book} />)}
+      {books.map(book => <Book key={book.bookId} book={book} />)}
     </tbody>
   </table>
 );
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   books: state.books,
 });
 
